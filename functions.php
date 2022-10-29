2db3c02108b0d7c0a9f96b575db45e48ff0c10e9
gabrielleles18 <gabriel.leles18@gmail.com>
2022-10-29 16:51:15 -0300

    Commit initial

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package medisite
+ * @since 1.0.0
+ */
+
+/**
+ * The theme version.
+ *
+ * @since 1.0.0
+ */
+define( 'MEDISITE_VERSION', wp_get_theme()->get( 'Version' ) );
