327ce784d7751f135c01845bdc0a393851fc1be1
gabrielleles18 <gabriel.leles18@gmail.com>
2022-11-01 12:59:30 -0300

    Enqueue styles

--- functions.php
+++ functions.php
@@ -13,4 +13,21 @@
  *
  * @since 1.0.0
  */
-define( 'MEDISITE_VERSION', wp_get_theme()->get( 'Version' ) );
+define('MEDISITE_VERSION', wp_get_theme()->get('Version'));
+
+add_action('wp_enqueue_scripts', function () {
+	wp_enqueue_style(
+		get_stylesheet() . '-style',
+		get_template_directory_uri() . '/root.css',
+		false,
+	);
+});
+
+add_action('enqueue_block_editor_assets', function () {
+	wp_enqueue_style(
+		get_stylesheet() . '-style-admin',
+		get_template_directory_uri() . '/root.css',
+		false,
+	);
+}, 1);
+
